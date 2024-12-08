82e55cd85676e96b65aacbf4f730407c7283adc2
Katerina <KaterinaDeeter@gmail.com>
2024-12-08 12:40:38 -0700

    Set homepage to index.html (added index.html and style.css)

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000; // Default to 5000 for local development
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
