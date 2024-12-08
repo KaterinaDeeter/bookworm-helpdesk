ccf510f1d339b2da12d368435f3ca6a6d16cba0a
KaterinaDeeter <katerinadeeter@gmail.com>
2024-12-08 11:31:38 -0700

    Attempt 863 (feeling hopeful)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000; // Default to 5000 for local development
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 console.log("Hello World")
