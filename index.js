503f070b099f53b2d5b47c001a7a751e0e540fe3
Katerina <KaterinaDeeter@gmail.com>
2024-12-08 12:32:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000; // Default to 5000 for local development
 
-app.get("/messages", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello");
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/:universalURL", (req, res) => {
-    res.send("404 URL NOT FOUND");
 });
 
 console.log("Hello World")
